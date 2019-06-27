0ac1fec49fb724d8775592164f63d9fab1a9f4b0
Mark Marvakh <mmarvakh@gmail.com>
2019-06-27 11:56:53 +0300

    Доп задание к первому уроку

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+let money = 100;
+let income = "early";
+let addExpenses = "";
+let deposit = "private";
+let mission = "hard";
+let period = 10;
+
+alert("First lesson");
+console.log("First lesson too");
 
