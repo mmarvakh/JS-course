5947ec61e2c28cf1c991892642d9cb5aae862836
Mark Marvakh <mmarvakh@gmail.com>
2019-06-28 11:49:42 +0300

    Второе домашнее задание

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-let money = 100;
-let income = "early";
-let addExpenses = "";
-let deposit = "private";
-let mission = "hard";
-let period = 10;
+let money = 93000;
+let income = "taxi";
+let addExpenses = "FOOD,WATER,MEDICATION";
+let deposit = true;
+let mission = 1500000;
+let period = 11;
+let budgetDay = money / 30
 
-alert("First lesson");
-console.log("First lesson too");
+console.log(typeof money, typeof income, typeof deposit);
+console.log(income.length);
+console.log("Период", period, "месяцев");
+console.log("Цель заработать", mission, "рублей");
+
+addExpenses = addExpenses.toLowerCase();
+//addExpenses = addExpenses.split([, [, 3]]);
+console.log(addExpenses.slice(0,4));
+console.log(addExpenses.slice(5,10));
+console.log(addExpenses.slice(11,21));
+console.log(budgetDay, (money % 30));   
 
