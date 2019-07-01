fb995be3f248436acd8be11ef0d9c8c9cd9a07c8
Mark Marvakh <mmarvakh@gmail.com>
2019-07-01 13:22:51 +0300

    Доп задание к третьему уроку

---
+++ appPlus.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Первое задание
+let lang;
+lang = prompt("Введите язык: 'ru' или 'en'");
+
+if (lang === "ru") {
+    console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
+} else {
+    console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+}
+
+switch (lang) {
+    case "ru":
+        console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
+        break;
+    case "en":
+        console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+}
+
+let array = [["Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье"],
+             ["Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"]];
+
+console.log((lang === "ru") ? (array[0][0]) : (array[1][0]));
+
+
+
+// Второе задание
+let namePerson = prompt("Введите имя:");
+console.log(namePerson === "Артем" ? "Директор" : namePerson === "Максим" ? "Преподаватель" : "Студент");
+
+
+
+
+
