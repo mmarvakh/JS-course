7b7ddb4ed18e159b7e7d79d5cb5dbac108646e52
Mark Marvakh <mmarvakh@gmail.com>
2019-07-09 13:41:02 +0300

    Доп задание к 8му уроку

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 'use strict';
 
-let week = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+let dateNull = function (a, b) { // a - day, b - month
+    b += 1;
 
-let date = new Date();
-date = date.getUTCDay();
-
-for (let i = 0; i < 7; i++) {
-    if (i === date) {
-        week[i] = week[i].bold();
-    } else if (i === 0 || i === 6) {
-        week[i] = week[i].italics();
-    } else {
-        week[i] = week[i];
+    if (a > 0 && a < 10) {
+        a = "0" + a;
     }
+    if (b > 0 && b < 10) {
+        b = "0" + b;
+    }
+    return a + "." + b;
 }
 
-document.writeln(week.join(", <br>"));
+let date = new Date();
+
+document.write(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " " + dateNull(date.getDate(), date.getMonth()) + "." + date.getFullYear());
+
