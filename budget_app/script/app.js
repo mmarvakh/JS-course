560ae3fcfb2c25fea1371c23147f7a3a5ad6c9ea
Mark Marvakh <mmarvakh@gmail.com>
2019-07-09 18:01:23 +0300

    Исправил ошибку

--- budget_app/script/app.js
+++ budget_app/script/app.js
@@ -9,7 +9,15 @@ let checkboxDeposit = document.querySelector("#deposit-check");
 
 let additionalIncome = document.querySelectorAll(".additional_income-item");
 
-let rightSide = document.querySelectorAll(".result");
+let budgetDay = document.getElementsByClassName("budget_day-value")[0],
+    budgetMonth = document.getElementsByClassName("budget_month-value")[0],
+    expensesMonth = document.getElementsByClassName("expenses_month-value")[0],
+    accumulatedMonth = document.getElementsByClassName("accumulated_month-value")[0],
+    additionalIncomeValue = document.getElementsByClassName("additional_income-value")[0],
+    additionalExpensesValue = document.getElementsByClassName("additional_expenses-value")[0],
+    incomePeriod = document.getElementsByClassName("income_period-value")[0],
+    targetMonth = document.getElementsByClassName("target_month-value")[0];
+
 
 let salaryAmount = document.querySelector(".salary-amount"),
     incomeTitle = document.querySelector(".income-title"),
@@ -19,6 +27,7 @@ let salaryAmount = document.querySelector(".salary-amount"),
     additionalExpenses = document.querySelector(".additional_expenses-item"),
     target = document.querySelector(".target-amount"),
     inputRange = document.querySelector(".period-select");
+
 
 
 
