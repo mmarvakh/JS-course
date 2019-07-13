3e6238d1a9a6da552339f26897ec9a610ab0a737
Mark Marvakh <mmarvakh@gmail.com>
2019-07-13 19:36:23 +0300

    Умер, но сделал

--- app.js
+++ app.js
@@ -19,9 +19,13 @@ DomElement.prototype.create = function () {
             width: 100px; \
             background-color: yellow; \
             font-size: 16px; \
-            position: absolute;";
+            position: absolute; \
+            margin: auto auto";
             block.textContent = this.selector;
             parent = document.body;
+            parent.style.cssText = "margin: 0 \
+            height: 500px; \
+            width: 500px;";
             parent.appendChild(block);
         } else if (this.selector[0] === "#") {
             block = document.createElement("p");
@@ -40,4 +44,29 @@ DomElement.prototype.create = function () {
 let DomElementChild = new DomElement(".Лиса");
 console.log(DomElementChild);
 DomElementChild.create();
+
+let blockMove = document.getElementsByTagName("div")[0];
+let top = 0,
+    left = 0,
+    right = 0,
+    bottom = 0;
+
+document.onkeydown = function (event) {
+    if (event.key === "ArrowRight") {
+        blockMove.style.left = left + "px";
+        left += 10;
+    }
+    if (event.key === "ArrowDown") {
+        blockMove.style.top = top + "px";
+        top += 10;
+    }
+    if (event.key === "ArrowUp") {
+        blockMove.style.bottom = bottom + "px";
+        bottom +=10;
+    }
+    if (event.key === "ArrowLeft") {
+        blockMove.style.right = right + "px";
+        right += 10;
+    }
+};
 });
