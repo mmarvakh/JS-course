49af521721d54c36cf856506c4dada97fcc10b15
Mark Marvakh <mmarvakh@gmail.com>
2019-07-13 14:16:52 +0300

    Основное задание

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-'use strict';
+"use strict";
 
-let dateNull = function (a, b) { // a - day, b - month
-    b += 1;
-
-    if (a > 0 && a < 10) {
-        a = "0" + a;
-    }
-    if (b > 0 && b < 10) {
-        b = "0" + b;
-    }
-    return a + "." + b;
+function DomElement (selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
 }
 
-let date = new Date();
+DomElement.prototype.create = function () {
+        let block;
+        let parent;
+        if (this.selector[0] === ".") {
+            block = document.createElement("div");
+            block.className = this.selector.slice(1);
+            block.style.cssText = "height: 100px; \
+            width: 100px; \
+            background-color: yellow; \
+            font-size: 16px;";
+            parent = document.body;
+            parent.appendChild(block);
+        } else if (this.selector[0] === "#") {
+            block = document.createElement("p");
+            block.className = this.selector.slice(1);
+            block.cssText = "height: 100px; \
+            width: 100px; \
+            background-color: yellow; \
+            font-size: 16px;";
+            parent = document.body;
+            parent.appendChild(block);
+        }
+    };
 
-document.write(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " " + dateNull(date.getDate(), date.getMonth()) + "." + date.getFullYear());
-
+let DomElementChild = new DomElement(".Лиса");
+console.log(DomElementChild);
+DomElementChild.create();
