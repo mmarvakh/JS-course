ae1194394f85bb01e74a661a4f7ee3c1710835ac
Mark Marvakh <mmarvakh@gmail.com>
2019-07-15 12:55:20 +0300

    Сотое исправление

--- app.js
+++ app.js
@@ -2,48 +2,50 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-function DomElement (selector, height, width, bg, fontSize) {
+let block;
+let parent;
+
+function DomElement (selector, height, width, bg, fontSize, margin, position) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    this.margin = margin;
+    this.position = position;
 }
 
 DomElement.prototype.create = function () {
-        let block;
-        let parent;
         if (this.selector[0] === ".") {
             block = document.createElement("div");
+            block.className = this.selector.slice(1);
+            block.style.cssText = `height: ${this.height}px; 
+            width: ${this.width}px; 
+            background-color: ${this.bg};
+            font-size: ${this.fontSize};
+            margin: auto auto;
+            position: absolute;`;
+            block.textContent = this.selector.slice(1);
+            parent = document.body;
+            parent.appendChild(block);
+        } 
+        if (this.selector[0] === "#") {
+            block = document.createElement("p");
             block.className = this.selector.slice(1);
             block.style.cssText = `${"height:" + this.height + ";"} \
             ${"width:" + this.width + ";"} \
             ${"background-color:" + this.bg + ";"} \
             ${"font-size:" + this.fontSize + ";"}`;
-            block.textContent = this.selector;
-            parent = document.body;
-            parent.style.cssText = "margin: 0 \
-            height: 500px; \
-            width: 500px;";
-            parent.appendChild(block);
-        } else if (this.selector[0] === "#") {
-            block = document.createElement("p");
-            block.className = this.selector.slice(1);
-            block.cssText = `${"height:" + this.height + ";"} \
-            ${"width:" + this.width + ";"} \
-            ${"background-color:" + this.bg + ";"} \
-            ${"font-size:" + this.fontSize + ";"}`;
-            block.textContent = this.selector;
+            block.textContent = this.selector.slice(1);
             parent = document.body;
             parent.appendChild(block);
         }
     };
 
-let DomElementChild = new DomElement(".Лиса", 100, 100, "yellow", "16px");
+let DomElementChild = new DomElement(".jija", 100, 100, "green", "16px");
 console.log(DomElementChild);
 DomElementChild.create();
 
-let blockMove = document.getElementsByTagName("div")[0];
 let top = 0,
     left = 0,
     right = 0,
@@ -51,19 +53,19 @@ let top = 0,
 
 document.onkeydown = function (event) {
     if (event.key === "ArrowRight") {
-        blockMove.style.left = left + "px";
+        block.style.left = left + "px";
         left += 10;
     }
     if (event.key === "ArrowDown") {
-        blockMove.style.top = top + "px";
+        block.style.top = top + "px";
         top += 10;
     }
     if (event.key === "ArrowUp") {
-        blockMove.style.bottom = bottom + "px";
+        block.style.bottom = bottom + "px";
         bottom +=10;
     }
     if (event.key === "ArrowLeft") {
-        blockMove.style.right = right + "px";
+        block.style.right = right + "px";
         right += 10;
     }
 };
