aab54c40ee1534b226e1dc2841437fa87c0b4391
Mark Marvakh <mmarvakh@gmail.com>
2019-08-01 14:51:10 +0300

    Задание для доп урока

---
+++ 3dglo/src/index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+import "@babel/polyfill";
+import 'nodelist-foreach-polyfill';
+import elementClosest from "element-closest";
+elementClosest(window);
+import "fetch-polyfill";
+import "es6-promise";
+import "formdata-polyfill";
+
+import countTimer from "./modules/countTimer";
+import toggleMenu from "./modules/toggleMenu";  
+import togglePopUp from "./modules/togglePopUp";  
+import tabs from "./modules/tabs";  
+import slider from "./modules/slider";  
+import team from "./modules/team";  
+import calc from "./modules/calc";  
+import sendForm from "./modules/sendForm";  
+import sendFormModal from "./modules/sendFormModal";  
+import sendFormFooter from "./modules/sendFormFooter";  
+
+// Timer
+countTimer("4 august 2019");
+
+// Menu
+toggleMenu();
+
+// Popup window
+togglePopUp();
+
+// Tabs
+tabs();
+
+// Slider
+slider();
+
+// Our team block
+team();
+
+// Calculator
+calc(100);
+
+// send-ajax-form
+sendForm();
+
+// send-ajax-form-modal
+sendFormModal();
+
+// send-ajax-form-footer
+sendFormFooter();
---
+++ 3dglo/src/modules/countTimer.js
@@ -0,0 +1,46 @@
+const countTimer = (deadLine) => {
+    let timerHours = document.querySelector("#timer-hours"),
+        timerMinutes = document.querySelector("#timer-minutes"),
+        timerSeconds = document.querySelector("#timer-seconds");
+
+    const getTimeRemaining = () =>  {
+        let dateStop = new Date(deadLine).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor((timeRemaining / 60) % 60),
+            hours = Math.floor(timeRemaining / 60 / 60);
+            return {timeRemaining, hours, minutes, seconds};
+    }
+    
+    const updateClock = () => {
+        let timer = getTimeRemaining();
+
+        if (timer.hours < 10) {
+            timer.hours = "0" + timer.hours;
+        }
+        if (timer.minutes < 10) {
+            timer.minutes = "0" + timer.minutes;
+        }
+        if (timer.seconds < 10) {
+            timer.seconds = "0" + timer.seconds;
+        }
+
+        timerHours.textContent = timer.hours;
+        timerMinutes.textContent = timer.minutes;
+        timerSeconds.textContent = timer.seconds;
+
+        if (timer.timeRemaining > 0) {
+            setInterval(updateClock, 1000);
+        }
+
+        if (timer.timeRemaining <= 0) {
+            timerHours.textContent = "00";
+            timerMinutes.textContent = "00";
+            timerSeconds.textContent = "00";
+        }
+    }
+    updateClock();
+};
+
+export default countTimer;
