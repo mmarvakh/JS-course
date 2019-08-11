658f4f1565e60823a9537284388f5b1dff5ef89e
Mark Marvakh <mmarvakh@gmail.com>
2019-08-11 19:40:58 +0300

    Сделана валидация данных в формах

--- Diplom/src/modules/validation.js
+++ Diplom/src/modules/validation.js
@@ -1,5 +1,16 @@
 const validation = () => {
+    const formInput = document.querySelectorAll("form>input");
 
+    formInput.forEach((elem) => {
+        elem.addEventListener("input", () => {
+            if (elem.getAttribute("name") === "user_name") {
+                elem.value = elem.value.replace(/[A-z\.\?,0-9\-\+=!@#№\$%\^&\*\.\/<>\?\(\)~`;:\(\)\/"']/gi, "");
+            }
+            if (elem.getAttribute("name") === "user_email") {
+                elem.value = elem.value.replace(/[А-я]/gi, "");
+            }
+        });
+    });
 };
 
 export default validation;
