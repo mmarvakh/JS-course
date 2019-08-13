10d45025d3d82afe044b6e21efc70a1ec257a8cb
Mark Marvakh <mmarvakh@gmail.com>
2019-08-13 18:46:37 +0300

    Сделан слайдер с отзывами

--- Diplom/src/modules/sliderReviews.js
+++ Diplom/src/modules/sliderReviews.js
@@ -1,17 +1,57 @@
 const sliderReviews = () => {
     const blockReviews = document.getElementById("ten"),
           slideReview = document.querySelectorAll(".sl2-slide"),
-          arrowRight = document.getElementById("arrow-right-two"),
-          arrowLeft = document.getElementById("arrow-left-two");
+          arrowsRight = document.querySelectorAll(".arrow-right-two"),
+          arrowsLeft = document.querySelectorAll(".arrow-left-two");
+
+    const arraySlideReview = [...slideReview];
 
     const removeSlide = () => {
-        for (let i = 0; i < slideReview.length; i++) {
+        for (let i = 0; i < arraySlideReview.length; i++) {
             if (i > 0) {
-                slideReview[i].style.display = "none"
+                arraySlideReview[i].style.display = "none"
             }
         };
     };
     removeSlide();
+
+    let currentSlide = 0;
+
+    const nextSlide = () => { 
+        arraySlideReview[currentSlide].style.display = "none";
+
+        currentSlide++;
+
+        if (currentSlide === 3) {
+            currentSlide = 0;
+        }
+
+        arraySlideReview[currentSlide].style.display = "block";
+    };
+
+    const prevSlide = () => {
+        arraySlideReview[currentSlide].style.display = "none";
+
+        currentSlide--;
+
+        if (currentSlide === -1) {
+            currentSlide = 2;
+        }
+
+        arraySlideReview[currentSlide].style.display = "block";
+    };
+
+    arrowsRight.forEach((elem) => {
+        elem.addEventListener("click", () => {
+            nextSlide();
+        });
+    });
+
+    arrowsLeft.forEach((elem) => {
+        elem.addEventListener("click", () => {
+            prevSlide();
+        });
+    });
 };
 
 export default sliderReviews;
